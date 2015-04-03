fa7f18dfd0ae232239957da54492fa2a094b1ef4
Michael Frame <michael.frame@SoteraDefense.com>
2015-04-03 10:33:02 -0500

    change db init to use new container, set startup script

--- deployment/prep_ubuntu.pl
+++ deployment/prep_ubuntu.pl
@@ -72,7 +72,7 @@ print "Starting Docker ...\n";
 print `sudo service docker start`;
 
 print "Getting and turning up 'all-in-one' docker container ...\n";
-print `cd src/Datawake/dev-env; sudo fig up -d ubuntu;`;
+print `cd src/Datawake/dev-env; sudo fig up -d datawakeone;`;
 
 print "Setting up MySQL database and creating test user ...\n";
 print `cd src/Datawake/dev-env; sudo ./init_db.sh;`;
--- dev-env/init_db.sh
+++ dev-env/init_db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "creating database tables"
-cat build_db.sql | docker run -i --link devenv_mysql_1:mysql --rm  mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" '
+cat build_db.sql | docker run -i --link devenv_datawakeone_1:mysql --rm  mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" '
 
