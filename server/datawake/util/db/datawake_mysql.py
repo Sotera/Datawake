e599651e8116e030db38f1f620c0a720ebc7feea
Brodie McDougald <brodie.mcdougald@soteradefense.com>
2015-07-09 08:17:57 -0500

    Minor styling changes, ReST set to true, and version number update

--- server/datawake/util/db/datawake_mysql.py
+++ server/datawake/util/db/datawake_mysql.py
@@ -31,11 +31,12 @@ import datetime
 from datawake.conf import datawakeconfig as dbconfig
 
 
-UseRestAPI = False
+UseRestAPI = True
 # StrongLoopHostname = 'localhost'
 StrongLoopHostname = os.environ['LOOPBACK_PORT_3001_TCP_ADDR']
 # StrongLoopPort = '5500'
 StrongLoopPort = os.environ['LOOPBACK_PORT_3001_TCP_PORT']
+
 
 """
 
